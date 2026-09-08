f7c46c01cd1b7d452211093bc736e27582c6dbdd
Melanie Prough <mprough@pro-webs.net>
2026-09-08 03:53:23 -0400

    Make package checks portable on GitHub Actions

--- tests/check-package.sh
+++ tests/check-package.sh
@@ -12,10 +12,10 @@ test -f "$version_root/admin/includes/functions/extra_functions/plugin_health_gu
 find "$root/files" -type f -name '*.php' -print0 | xargs -0 -n1 php -l >/dev/null
 php -d zend.assertions=1 -d assert.exception=1 "$root/tests/scanner-test.php"
 
-rg -q "'pluginVersion' => 'v1.0.1'" "$version_root/manifest.php"
-rg -q "public string \\$version = '1.0.1'" "$version_root/Installer/ScriptedInstaller.php"
-rg -q "'PLUGIN_HEALTH_GUARD_VERSION', '1.0.1'" "$version_root/Installer/ScriptedInstaller.php"
-if rg -q '^\\$installedPlugins\\s*=' "$version_root/admin/plugin_health_guard.php"; then
+grep -Fq "'pluginVersion' => 'v1.0.1'" "$version_root/manifest.php"
+grep -Fq "public string \\$version = '1.0.1'" "$version_root/Installer/ScriptedInstaller.php"
+grep -Fq "'PLUGIN_HEALTH_GUARD_VERSION', '1.0.1'" "$version_root/Installer/ScriptedInstaller.php"
+if grep -Eq '^\$installedPlugins[[:space:]]*=' "$version_root/admin/plugin_health_guard.php"; then
     echo 'Admin page must not overwrite Zen Cart bootstrap variable $installedPlugins.' >&2
     exit 1
 fi
